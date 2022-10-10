13847368eaf6ebff3db992a71485457b55434fbc
zeeshanshabbeer <z6680362@gmail.com>
2022-10-10 12:00:18 +0500

    timetable controller

--- routes/TimetableRouter.js
+++ routes/TimetableRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const S_authController = require("../Controllers/S_authController");
-const timetableController = require("../Controllers/timetableController");
+const timetableController = require("../Controllers/TimetableController");
 //add timetable in database
 router.post("/Timetable", timetableController.timetable);
 //remove clashes
